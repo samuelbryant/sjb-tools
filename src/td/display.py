5a09836f9ab9e13d9abadb730723f8e397f30b29
sambryant <samuel.bryant@yale.edu>
2018-04-05 11:57:40 -0400

    minor docstring update

--- src/td/display.py
+++ src/td/display.py
@@ -5,7 +5,6 @@ import td.classes
 def _repr_tags(tags):
   return((tags and '#' + ', #'.join(tags)) or '')
 
-
 def _repr_priority(priority):
   if priority is td.classes.PriorityEnum.DEFAULT.value:
     return ' '
@@ -17,20 +16,17 @@ def _repr_priority(priority):
     raise td.classes.ProgrammingError(
       'display._repr_priority', 'Illegal priority argument: '+str(priority))
 
-
-def repr_todo(todo):
+def repr_todo(todo, simple=False):
   """Returns a string reprentation of a todo item.
   
-  This method returns things as:
-  53  ! This is the todo item text #tag1,#tag2
-
+  This outputs todo items with the following format:
+    53  ! This is the todo item text #tag1,#tag2
   Where 53 is the ID, the '!' is because the item is Urgent, and tag1,tag2 are the tags.
 
   Returns:
     str: String representation of a todo item.
   """
   return '%-3d %1s %s %s' % (todo.id, _repr_priority(todo.priority), todo.text, _repr_tags(todo.tags))
-
 
 def display_todo(todo):
   print(repr_todo(todo))
