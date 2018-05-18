8678fe847563f9fcf1cdb9ebd8f72a18feb95b81
Samuel Bryant <samuel.bryant@yale.edu>
2018-05-18 16:39:35 -0400

    setup.py in a semi-working state. Tested on virtual box

--- setup.py
+++ setup.py
@@ -1,24 +1,24 @@
 from distutils.core import setup
 import os
-import git
 from os import path
 
 here = path.abspath(path.dirname(__file__))
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+with open(path.join(here, 'README.md')) as f:
   long_description = f.read()
+
+
+
 
 setup(
     name='sjb-todo',
     author='Sam Bryant',
-    version_format='{tag}.dev{commitcount}+{gitsha}',
-    setup_requires=['setuptools-git-version'],
+    version='0.1.1.4',
     description='A simple CLI program to create, maintain and edit todo lists',
     long_description=long_description,
-    long_description_content_type='text/markdown',
+    #long_description_content_type='text/markdown',
     url='https://github.com/sicklybeans/sjb_todo',
-    # TODO
     package_dir={'': 'lib'},
-    py_modules=['sjb.td', 'sjb.common'],
+    packages=['sjb','sjb.td','sjb.common'],
     scripts=['bin/sjb-todo'],
     ## packages=['sjb',],
     license='MIT License'
