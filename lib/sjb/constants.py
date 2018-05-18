b6a8492644bd59c3569a9326750163d8d9bbcf27
sambryant <samuel.bryant@yale.edu>
2018-05-18 18:45:30 -0400

    Getting very close to deployability

--- lib/sjb/constants.py
+++ lib/sjb/constants.py
@@ -1,4 +1,4 @@
 program = 'sjb-todo'
 description = 'A simple CLI program to create, maintain and edit todo lists'
 __author__ = 'Sam Bryant'
-__version__ = 'v0.1.2dev-9'
+__version__ = 'v0.1.2.dev10'
--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
-from distutils.core import setup
+#!/usr/bin/env python3.6
+from setuptools import setup
 import os
+import sys
 from os import path
 
 here = path.abspath(path.dirname(__file__))
@@ -9,11 +11,19 @@ with open(path.join(here, 'README.md')) as f:
 # Single source important information
 exec(open('lib/sjb/constants.py').read())
 
+if sys.version_info[0] != 3:
+    print('This script requires Python 3')
+    exit(1)
+
 setup(
     name=program,
     author=__author__,
     version=__version__,
     description=description,
+    install_requires=[
+        'future>=0.16.0',
+        'configobj >= 5.0.6'
+    ],
     long_description=long_description,
     url='https://github.com/sicklybeans/sjb_todo',
     package_dir={'': 'lib'},
