741fc8ad0ca1f6b3bac3c81c18618c7a5b4b28c5
sambryant <samuel.bryant@yale.edu>
2018-05-19 16:01:01 -0400

    Fixed bug in setup_dev script

--- scripts/setup_dev.sh
+++ scripts/setup_dev.sh
@@ -6,14 +6,31 @@
 # you will run the version of the application given by the local source code.
 # This is to allow for local application development without conflicting with
 # installation.
-
-echo "This script must be sourced"
-
 DEV_ENV_NAME="venv"
+THIS_SCRIPT="scripts/setup_dev.sh"
 
-# Go to project directory
-cd "$(dirname "$0")/.."
+# Use while loop so we can break out without exiting a sourced script.
+while [[ "1" == "1" ]]; do
+  # Check that script is sourced.
+  if [[ ! $0 =~ /bash$ ]] && [[ ! $0 =~ /sh$ ]] && [[ ! $0 =~ /zsh$ ]]; then
+    echo "Error: this script must be sourced (from bash, sh, or zsh)" > /dev/stderr
+    break
+  fi
 
-python3.6 -m venv "$DEV_ENV_NAME"
-source "./$DEV_ENV_NAME/bin/activate"
-python setup.py install
+  # Ensure we are in the project directory by checking for the '.git'
+  if [[ ! -d "$PWD/.git" ]] || [[ ! -f "$PWD/$THIS_SCRIPT" ]]; then
+    echo "Error: Please source this script from the project directory" > /dev/stderr
+    break
+  fi
+
+  # Create virtual environment
+  python3.6 -m venv "$DEV_ENV_NAME"
+  if [[ "$?" -ne "0" ]]; then break; fi
+  source "./$DEV_ENV_NAME/bin/activate"
+  if [[ "$?" -ne "0" ]]; then break; fi
+  python setup.py install
+  if [[ "$?" -ne "0" ]]; then break; fi
+
+  echo "Dev environment setup was successful. To leave type 'deactivate'"
+  break
+done
